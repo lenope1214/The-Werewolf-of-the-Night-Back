37d01a181232e685c057f8fe93d53daedc96c646
이성복 <dltjdqhr1000@gmail.com>
2023-02-07 22:46:59 +0900

    [FIX] springdoc-openapi v2로 바뀌면서 패키지 변경

--- api/src/main/kotlin/kr/weareboard/werewolf/api/config/swagger/SwaggerConfig.kt
+++ api/src/main/kotlin/kr/weareboard/werewolf/api/config/swagger/SwaggerConfig.kt
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
-import org.springdoc.core.GroupedOpenApi
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
